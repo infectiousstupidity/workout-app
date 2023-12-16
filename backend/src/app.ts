ccf2209ad66b97ff1859c229c978bf31e65759bc
infectiousstupidity <sub@fakeout.no>
2023-12-16 02:02:30 +0100

    Implement Morgan for Enhanced HTTP Request Logging
    - Added requestLogger in utilities/logger for custom Morgan setup.
    - Configured Morgan to utilize Winston for detailed HTTP request logging.
    - Integrated requestLogger into app.ts to log all incoming requests.
    - Ensured Morgan logging is compatible with existing error handling and authentication.
    - Tailored logging format for comprehensive request insights, aiding in monitoring and debugging.
    - Deleted unused files.
    
    This update adds advanced logging capabilities to the workout app, providing a clear overview of HTTP traffic for development and maintenance purposes.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-import morgan from 'morgan';
-import logger from './utilities/logger/logger';
+import requestLogger from './utilities/logger/request-logger';
 import helmetConfig from './utilities/security/helmet';
 import passport from './auth/passport-config';
 import authRoutes from './auth/auth-routes';
@@ -13,18 +12,11 @@ const app = express();
 dotenv.config();
 
 app.use(helmetConfig);
-
-app.use(
-  morgan('combined', { stream: { write: (message) => logger.info(message) } }),
-);
-
+app.use(requestLogger);
 app.use(express.json());
-
 app.use(passport.initialize());
-
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
-
 app.use(handleError);
 
-export default passport;
+export default app;
--- backend/src/utilities/logger/request-logger.ts
+++ backend/src/utilities/logger/request-logger.ts
@@ -1,1 +1,18 @@
-/* morgan morgan */
+import morgan, { type StreamOptions } from 'morgan';
+import logger from './logger';
+
+const stream: StreamOptions = {
+  write: (message) => logger.info(message.trim()),
+};
+
+const skip = (): boolean => {
+  const env = process.env.NODE_ENV != null || 'development';
+  return env !== 'development';
+};
+
+const requestLogger = morgan(
+  ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms',
+  { stream, skip },
+);
+
+export default requestLogger;
